eac95f58aafe94e935473c5dd1c519f7ea8acf51
DerKO <dr.knockout99@gmail.com>
2019-08-19 23:02:33 -0600

    Moved to WorldViews folder and namespace. Added filtered datagrids

---
+++ YLSaveFileEditor/YLSaveFileEditor/WorldViews/CapitalCashinoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YLSaveFileEditor.WorldViews
+{
+    /// <summary>
+    /// Interaction logic for CapitalCashinoView.xaml
+    /// </summary>
+    public partial class CapitalCashinoView : UserControl
+    {
+        public CapitalCashinoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
