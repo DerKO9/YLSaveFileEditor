f17084bb0b9649f163f8543795b6338ed96f1ad6
DerKO <dr.knockout99@gmail.com>
2019-08-20 19:34:56 -0600

    Moved GameStat to own file

---
+++ YLSaveFileEditor/YLSaveFileEditor/GameStat.cs
@@ -0,0 +1,19 @@
+﻿namespace YLSaveFileEditor
+{
+    public partial class MainWindow
+    {
+        public class GameStat
+        {
+            public GameStat(int id, string name, int value)
+            {
+                Id = id;
+                Name = name;
+                Value = value;
+            }
+            public int Id { get; }
+            public string Name { get; }
+            public int Value { get; set; }
+        }
+
+    }
+}
--- YLSaveFileEditor/YLSaveFileEditor/MainWindow.xaml.cs
+++ YLSaveFileEditor/YLSaveFileEditor/MainWindow.xaml.cs
@@ -122,18 +122,5 @@ namespace YLSaveFileEditor
             DataEditView.IsEnabled = true;
         }
 
-        public class GameStat
-        {
-            public GameStat(int id, string name, int value)
-            {
-                Id = id;
-                Name = name;
-                Value = value;
-            }
-            public int Id { get; }
-            public string Name { get; }
-            public int Value { get; set; }
-        }
-
     }
 }
